cb05debaa430ee9091a45dd3cdade40d5d5e50db
paola.kloch <kloch.157@gmail.com>
2024-01-31 21:55:22 -0300

    prettier

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -6,36 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  cards = [
-    {
-      src: 'assets/images/conscientizacao.png',
-      title: 'Conscientização',
-      description: ` Acesse conteudos de que abordam identificação de sinais de estresse, ansiedade e depressão, além de fornecer estratégias de enfrentamento.`,
-      actionText: 'Treinamentos',
-    },
-    {
-      src: 'assets/images/acesso-a-recursos.png',
-      title: 'Acesso a recursos',
-      description: ` Contamos com profissionais, aplicativos e materiais online, para oferecer suporte abrangente à saúde mental, incluindo listas de leituras e vídeos.`,
-      actionText: 'Experimente aqui',
-    },
-    {
-      src: 'assets/images/avaliacoes.png',
-      title: 'Avaliações Regulares',
-      description: ` Pesquisas para compreender as necessidades individuais dos colaboradores, abordando níveis de estresse, satisfação e bem-estar no trabalho.`,
-      actionText: 'Avalie também!',
-    },
-    {
-      src: 'assets/images/apoio.png',
-      title: ' Cultura de Apoio',
-      description: ` Encorajamos uma atmosfera de abertura e suporte, onde os colaboradores se sintam à vontade para compartilhar suas preocupações.`,
-      actionText: 'Opine',
-    },
-  ];
-
-  quote = {
-    imageSrc: 'https://www.rpinfo.com.br/views/geral/img/aspas.png',
-    text: 'Só a experiência própria é capaz de tornar sábio o ser humano.',
-    author: 'Sigmund Freud',
-  };
+  cards = [{}, {}, {}, {}];
 }
+// import {Component} from '@angular/core';
+// import {MatIconModule} from '@angular/material/icon';
+
+// /**
+//  * @title Basic icons
+//  */
+// @Component({
+//   selector: 'icon-overview-example',
+//   templateUrl: 'icon-overview-example.html',
+//   standalone: true,
+//   imports: [MatIconModule],
+// })
+// export class IconOverviewExample {}
